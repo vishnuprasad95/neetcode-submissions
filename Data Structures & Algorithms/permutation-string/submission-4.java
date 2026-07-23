b6a39cbb04515de32c048a6fc786c02158234e08
vishnuprasad95 <39374580+vishnuprasad95@users.noreply.github.com>
2026-07-23 22:48:14 +0530

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length())
+        return false;
+        int[] freqS1= new int[26];
+        int[] freqWindow= new int[26];
+        char[] s1Array= s1.toCharArray();
+        char[] s2Array= s2.toCharArray();
+        int right=s1Array.length;
+        for(char a:s1Array)
+        {
+            freqS1[a-'a']++;
+        }
+        for(int i=0;i<s1Array.length;i++)
+        {
+            freqWindow[s2Array[i]-'a']++;
+        }
+        if(Arrays.equals(freqS1,freqWindow))
+          return true;
+        for(int left=0;right<s2.length();)
+        {
+          freqWindow[s2Array[left]-'a']--;
+          left++;
+          freqWindow[s2Array[right]-'a']++;
+          right++;
+                   if(Arrays.equals(freqS1,freqWindow))
+          return true;
+        }
+        return false;
+
+    }
+}
